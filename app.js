b0f0bdc8b9fcd2c5bb7ee42967732547df8e0f42
Asad Zubair <asad.xnb@gmail.com>
2025-12-01 00:37:35 +0500

    divided the pages into includes

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const path = require('path');
 
+const baseRoutes = require('./routes/base.routes');
+
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
-app.get("/", (req, res) => {
-    res.render("dashboard");
-})
-
-
+app.use('/', baseRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
---
+++ routes/base.routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+router.get("/", (req, res) => {
+    res.render("dashboard");
+});
+
+router.get("/enrollment", (req, res) => {
+    res.render("enrollment");
+});
+
+router.get("/reports", (req, res) => {
+    res.render("reports");
+});
+
+router.get("/settings", (req, res) => {
+    res.render("settings");
+});
+
+router.get("/terminal", (req, res) => {
+    res.render("terminal");
+});
+
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
+
+module.exports = router;
