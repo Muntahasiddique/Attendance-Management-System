18bc2dbe3d7cf9ac39d3c69e65c037b0b7ca67a0
Asad Zubair <asad.xnb@gmail.com>
2025-12-01 19:49:51 +0500

    changed active nav class, dynamically

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get("/", (req, res) => {
+    res.redirect("/dashboard");
+});
+
 router.get("/dashboard", (req, res) => {
     res.render("dashboard");
 });
